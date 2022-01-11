6b2c7ad61b55922db3444a42f356e3a5d66a9331
Bartek S <bartlomiej.stoklosa@netguru.com>
2022-01-11 20:56:17 +0100

    Fix production video

--- app.py
+++ app.py
@@ -48,4 +48,5 @@ webrtc_streamer(
     key="RockPaperScissor",
     video_processor_factory=VideoProcessor,
     media_stream_constraints={"video": True, "audio": False},
+    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
 )
